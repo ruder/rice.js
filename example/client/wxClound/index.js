07fc2cfcd462447caf56eb45a88f261dc3e14785
myruder <rudermail@qq.com>
2020-07-06 09:11:28 +0800

    fix wxcloudchannel

--- example/client/wxClound/index.js
+++ example/client/wxClound/index.js
@@ -2,6 +2,6 @@ let Channel=require("../../../client/wx-cloud-channel")
 let Rice=require("../../../client/lib")
 let key="123456"
 
-let rice = Rice.create( new Channel("rice"), key )
+let rice = Rice.create( new Channel("rice","default"), key )
 
 module.exports = rice;

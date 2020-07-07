97b930dc7b62df409c7daae271b4975337cb8426
myruder <rudermail@qq.com>
2020-07-07 16:33:02 +0800

    remove wxClient client key

--- example/client/wxClound/index.js
+++ example/client/wxClound/index.js
@@ -1,6 +1,5 @@
 let Channel=require("@ricejs/client-wx-cloud-channel")
-let Rice=require("@ricejs/client")
-//这个key必须要(sever)服务端保持一致。可以为空，为空则传输不加密 
+let Rice=require("@ricejs/client") 
 
 let rice = Rice.create(new Channel("rice","default"))
 
